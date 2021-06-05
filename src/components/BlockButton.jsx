b155c28e5c1b056732633c80aa7c9ae5965da293
Omer <27678413+TheOmer77@users.noreply.github.com>
2021-06-05 19:43:44 +0300

    Add Android 12 like "Block buttons"

---
+++ src/components/BlockButton.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+// eslint-disable-next-line no-unused-vars
+import Button, { ButtonProps } from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingBlock: theme.spacing(2),
+    paddingInline: theme.spacing(3.5),
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  rootVertical: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
+    fontSize: theme.typography.overline.fontSize,
+    lineHeight: '.875rem',
+  },
+  verticalLabel: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  verticalIcon: {
+    marginInline: 0,
+    marginBlockEnd: theme.spacing(0.5),
+  },
+}));
+
+/** @param {{vertical: boolean} & ButtonProps} props */
+const BlockButton = ({ children, vertical = false, ...props }) => {
+  const classes = useStyles();
+  return (
+    <Button
+      {...props}
+      classes={{
+        root: vertical ? classes.rootVertical : classes.root,
+        label: vertical ? classes.verticalLabel : null,
+        startIcon: vertical ? classes.verticalIcon : null,
+        endIcon: vertical ? classes.verticalIcon : null,
+      }}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default BlockButton;
