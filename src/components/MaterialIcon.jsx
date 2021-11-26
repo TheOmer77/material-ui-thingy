6579dbe1afa1f1ccbb5d873c2429d4077c3dcdb5
Omer <27678413+TheOmer77@users.noreply.github.com>
2021-11-26 22:29:45 +0200

    Use all given props in MaterialIcon

--- src/components/MaterialIcon.jsx
+++ src/components/MaterialIcon.jsx
@@ -3,7 +3,9 @@ import classNames from 'classnames';
 
 /** @param {{ iconName: string, className: string } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>} props */
 const MaterialIcon = ({ iconName, className, ...props }) => (
-  <i className={classNames('material-icons', className)}>{iconName}</i>
+  <i className={classNames('material-icons', className)} {...props}>
+    {iconName}
+  </i>
 );
 
 export default MaterialIcon;
