899e428064979b599a9bb209e9c9e2821dea4200
Omer <27678413+TheOmer77@users.noreply.github.com>
2021-11-27 18:28:05 +0200

    Use ModalSheet component in ControlSheet

--- src/components/ControlSheet.jsx
+++ src/components/ControlSheet.jsx
@@ -1,20 +1,18 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import classNames from 'classnames';
 
 import { makeStyles } from '@material-ui/core';
-import Backdrop from '@material-ui/core/Backdrop';
-import Container from '@material-ui/core/Container';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import Fab from '@material-ui/core/Fab';
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import Paper from '@material-ui/core/Paper';
 import Switch from '@material-ui/core/Switch';
-import Typography from '@material-ui/core/Typography';
 
+import ModalSheet from './ModalSheet';
 import MaterialIcon from './MaterialIcon';
+
 import useGlobalStates from '../hooks/useGlobalStates';
 
 const CATEGORY_IDS = { APP_BAR: 'appBar', BLOCK_BTNS: 'blockBtns' };
@@ -25,28 +23,9 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: theme.palette.common.white,
-  },
   extendedIcon: { marginRight: theme.spacing(1) },
-  container: {
-    position: 'fixed',
-    bottom: 0,
-    paddingInline: 0,
-    [theme.breakpoints.up('sm')]: { paddingInline: theme.spacing(3) },
-  },
-  sheet: {
-    width: '100%',
-    borderRadius: `${theme.shape.borderRadius}px ${theme.shape.borderRadius}px 0 0`,
-    paddingBottom: theme.spacing(1),
-    transform: 'translateY(100%)',
-    transition: `transform ${theme.transitions.duration.short}ms ${theme.transitions.easing.easeInOut}`,
-  },
-  sheetOpen: { transform: 'translateY(0%)' },
-  sheetTitle: { padding: `${theme.spacing(1)}px ${theme.spacing(2)}px` },
-  list: { padding: 0 },
-  listSubheader: { position: 'relative' },
+  list: { paddingTop: 0 },
+  listSubheader: { position: 'relative', lineHeight: `${theme.spacing(4)}px` },
 }));
 
 /**
@@ -128,57 +107,43 @@ const ControlSheet = ({ currentCategory }) => {
         <MaterialIcon iconName='settings' className={classes.extendedIcon} />
         Options
       </Fab>
-      <Backdrop
-        open={open}
-        onClick={() => setOpen(false)}
-        onKeyDown={(e) => console.log(e)}
-        className={classes.backdrop}
-      >
-        <Container className={classes.container}>
-          <Paper
-            className={classNames(classes.sheet, open && classes.sheetOpen)}
-            onClick={(e) => e.stopPropagation()}
+      <ModalSheet open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Options</DialogTitle>
+        {categories.map((category) => (
+          <List
+            className={classes.list}
+            subheader={
+              <ListSubheader className={classes.listSubheader}>
+                {category.label}
+              </ListSubheader>
+            }
+            key={category.id}
           >
-            <Typography variant='h6' className={classes.sheetTitle}>
-              Options
-            </Typography>
-            {categories.map((category) => (
-              <List
-                className={classes.list}
-                subheader={
-                  <ListSubheader className={classes.listSubheader}>
-                    {category.label}
-                  </ListSubheader>
-                }
-                key={category.id}
-              >
-                {listItems
-                  .filter((item) => item.category === category.id)
-                  .map(
-                    ({ id, category, label, state: [state, setState] }) =>
-                      (!currentCategory || currentCategory === category) && (
-                        <ListItem
-                          button
-                          key={id}
-                          onClick={() => setState((prev) => !prev)}
-                        >
-                          <ListItemText primary={label} />
-                          <ListItemSecondaryAction>
-                            <Switch
-                              edge='end'
-                              checked={state}
-                              name={id}
-                              onChange={() => setState((prev) => !prev)}
-                            />
-                          </ListItemSecondaryAction>
-                        </ListItem>
-                      )
-                  )}
-              </List>
-            ))}
-          </Paper>
-        </Container>
-      </Backdrop>
+            {listItems
+              .filter((item) => item.category === category.id)
+              .map(
+                ({ id, category, label, state: [state, setState] }) =>
+                  (!currentCategory || currentCategory === category) && (
+                    <ListItem
+                      button
+                      key={id}
+                      onClick={() => setState((prev) => !prev)}
+                    >
+                      <ListItemText primary={label} />
+                      <ListItemSecondaryAction>
+                        <Switch
+                          edge='end'
+                          checked={state}
+                          name={id}
+                          onChange={() => setState((prev) => !prev)}
+                        />
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  )
+              )}
+          </List>
+        ))}
+      </ModalSheet>
     </>
   );
 };
