b2c67e453fc2916b3a5d33ec46e3b557ecf903b5
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-03-25 20:49:16 +0300

    More tweaks to desktop layout
    My commit messages are getting worse 😑

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import classNames from 'classnames';
 
 import { styled, Theme, useMediaQuery } from '@mui/material';
 import Button from '@mui/material/Button';
@@ -6,12 +7,17 @@ import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import MuiAppBar from '@mui/material/AppBar';
+import MuiToolbar from '@mui/material/Toolbar';
 import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 import AppBar from 'components/AppBar';
 import { TestList } from 'components/test';
 
 import listItems from 'constants/listItems';
+
+import trianglesBg from 'assets/triangles-bg.svg';
 
 const StyledContainer = styled('div')(({ theme }) => ({
   width: '100%',
@@ -22,16 +28,31 @@ const StyledContainer = styled('div')(({ theme }) => ({
 
   '& .scrolling-container': {
     overflowY: 'auto',
-    height: `calc(100vh - ${theme.spacing(8)})`,
-    '@supports (height: 100dvh)': {
-      height: `calc(100dvh - ${theme.spacing(8)})`,
+    '&.MuiList-root': {
+      height: `calc(100vh - ${theme.spacing(8)})`,
+      '@supports (height: 100dvh)': {
+        height: `calc(100dvh - ${theme.spacing(8)})`,
+      },
     },
+  },
+  '& .list-container': {
+    minWidth: '50vw',
+    [theme.breakpoints.up('lg')]: { minWidth: theme.spacing(64) },
+  },
+  '& .content-container': {
+    flexGrow: 1,
+    padding: theme.spacing(2, 0),
+    height: '100vh',
+    '@supports (height: 100dvh)': { height: '100dvh' },
 
-    '&.list-container': {
-      minWidth: '50vw',
-      [theme.breakpoints.up('lg')]: { minWidth: theme.spacing(64) },
+    '&:not(.selected-item-content)': {
+      backgroundColor:
+        theme.palette.mode === 'dark'
+          ? theme.palette.background.default
+          : theme.palette.primary.main,
+      backgroundImage: `url("${trianglesBg}")`,
+      backgroundPosition: 'center',
     },
-    '&.content-container': { flexGrow: 1, padding: theme.spacing(2, 0) },
   },
 }));
 
@@ -43,14 +64,25 @@ const App = () => {
 
   return (
     <>
-      <AppBar title='App' collapsing={!mdBreakpoint}>
+      <AppBar
+        title={mdBreakpoint ? '' : 'App'}
+        collapsing={!mdBreakpoint}
+        addPadding={!mdBreakpoint}
+        color={mdBreakpoint ? 'transparent' : 'primary'}
+        toolbarColor='primary'
+      >
         <Button>Login</Button>
       </AppBar>
       <StyledContainer>
         {mdBreakpoint ? (
           <>
-            <Paper className='scrolling-container list-container' square>
-              <List>
+            <Paper className='list-container' square>
+              <MuiAppBar position='static'>
+                <MuiToolbar>
+                  <Typography variant='h6'>App</Typography>
+                </MuiToolbar>
+              </MuiAppBar>
+              <List className='scrolling-container'>
                 {listItems.map(({ primaryText, secondaryText }, index) => (
                   <ListItemButton
                     key={index}
@@ -65,10 +97,17 @@ const App = () => {
                 ))}
               </List>
             </Paper>
-            <div className='scrolling-container content-container'>
+            <div
+              className={classNames(
+                'scrolling-container',
+                'content-container',
+                typeof selectedItem === 'number' && 'selected-item-content'
+              )}
+            >
               <Container>
-                {typeof selectedItem === 'number' &&
-                  listItems[selectedItem].content}
+                {typeof selectedItem === 'number' && (
+                  <>{listItems[selectedItem].content}</>
+                )}
               </Container>
             </div>
           </>
